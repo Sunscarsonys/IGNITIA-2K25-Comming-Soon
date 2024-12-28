9f4f8de73167cd1bcb5aa06ccc1f4c9d6568a82f
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-28 22:10:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <div className="logo">
-        <img src="/images/info-btn.png" alt="Info" />
+      <div className="logo-container">
+        <div className="rotating-circle">
+          <img src="/images/inner-2.png" alt="Rotating Circle" />
+        </div>
+        <div className="inner-logo">
+          <img src="/images/inner-1.png" alt="Inner Logo" />
+        </div>
       </div>
-      <div className="ignitia-logo">
-        <img src="/images/ignitia-logo.png" alt="Ignitia Logo" />
+
+      <div className="top-right-image">
+        <img src="/images/ignitia-logo.png" alt="Top Right Image" />
       </div>
+
       <div className="slideshow">
         <div className="slide slide1"></div>
         <div className="slide slide2"></div>
         <div className="slide slide3"></div>
       </div>
+
+      <div className="hero-image">
+        <img src="/images/ignitia-logo.png" alt="Hero" />
+      </div>
+
       <div className="glitch-text">
         <div className="container">
           <div className="stack" style={{ "--stacks": 3 }}>
